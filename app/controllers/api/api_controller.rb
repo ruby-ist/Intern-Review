5172168ca1a02a6b91bc13962bb428d578710660
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2023-03-24 17:46:57 +0530

    Seperate filter action for API

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -6,4 +6,16 @@ class Api::ApiController < ApplicationController
 	def current_account
 		@current_account ||= Account.find doorkeeper_token[:resource_owner_id]
 	end
+
+	def not_an_intern_account!
+		if current_account.intern?
+			render json: {errors: {account: "does not have that privilege"}}, status: :non_authoritative_information
+		end
+	end
+
+	def admin_account!
+		unless current_account.admin_user?
+			render json: {errors: {account: "does not have that privilege"}}, status: :non_authoritative_information
+		end
+	end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,18 @@ class ApplicationController < ActionController::Base
 	end
 
 	def not_an_intern_account!
-		unless account_signed_in? || current_account
+		unless account_signed_in?
 			authenticate_account!
 		end
 
 		if current_account.intern?
-			redirect_back fallback_location: "/dashboard" ,alert: "You are not authorised to do that!"
+			redirect_back fallback_location: "/courses" ,alert: "You are not authorised to do that!"
 		end
 	end
 
 	def admin_account!
 		unless current_account.admin_user?
-			redirect_back fallback_location: "/dashboard" ,alert: "You are not authorised to do that!"
+			redirect_back fallback_location: "/courses" ,alert: "You are not authorised to do that!"
 		end
 	end
 end
