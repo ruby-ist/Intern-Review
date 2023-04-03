375072261bd505be7b051b2b859b384a1e5eb7c2
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2023-04-03 17:18:10 +0530

    Removed instance variables in test and implemented let!

--- spec/models/daily_report_spec.rb
+++ spec/models/daily_report_spec.rb
@@ -78,48 +78,45 @@ RSpec.describe DailyReport do
 	end
 
 	context "for first report of the section" do
-		before do
-			@section_report = create(:section_report)
-			expect(@section_report.daily_reports).to be_empty
+		let(:section_report) { create(:section_report) }
+
+		it "has no daily reports before" do
+			expect(section_report.daily_reports).to be_empty
 		end
 
 		context "on creation" do
 			context "ongoing report" do
 				it "should update start date" do
-					daily_report = create(:daily_report, section_report: @section_report, status: "ongoing")
-					expect(@section_report.start_date).to eql(daily_report.date)
-					expect(@section_report.end_date).to be nil
+					daily_report = create(:daily_report, section_report: section_report, status: "ongoing")
+					expect(section_report.start_date).to eql(daily_report.date)
+					expect(section_report.end_date).to be nil
 				end
 			end
 
 			context "completed report" do
-				before do
-					@daily_report = create(:daily_report, section_report: @section_report, status: "completed")
-				end
+				let!(:daily_report) { create(:daily_report, section_report: section_report, status: "completed") }
 
 				it "should update section report start date" do
-					expect(@section_report.start_date).to eql(@daily_report.date)
+					expect(section_report.start_date).to eql(daily_report.date)
 				end
 
 				it "should update section report end date" do
-					expect(@section_report.end_date).to eql(@daily_report.date)
+					expect(section_report.end_date).to eql(daily_report.date)
 				end
 
 				it "should update section report status" do
-					expect(@section_report.completed?).to be true
+					expect(section_report.completed?).to be true
 				end
 			end
 		end
 
 		context "on update" do
-			before do
-				daily_report = create(:daily_report, section_report: @section_report, date: Date.today)
-				daily_report.update(date: Date.yesterday)
-			end
+			let(:daily_report) { create(:daily_report, section_report: section_report, date: Date.today) }
 
 			context "report date" do
 				it "should match section report start date" do
-					expect(@section_report.start_date).to eql Date.yesterday
+					daily_report.update(date: Date.yesterday)
+					expect(section_report.start_date).to eql Date.yesterday
 				end
 			end
 		end
@@ -127,55 +124,52 @@ RSpec.describe DailyReport do
 		context "on deleting" do
 			context "only report" do
 				it "should update section report dates" do
-					daily_report = create(:daily_report, section_report: @section_report, status: "completed")
-					expect(@section_report.start_date).not_to be_nil
-					expect(@section_report.end_date).not_to be_nil
-					expect(@section_report.completed?).to be true
+					daily_report = create(:daily_report, section_report: section_report, status: "completed")
+					expect(section_report.start_date).not_to be_nil
+					expect(section_report.end_date).not_to be_nil
+					expect(section_report.completed?).to be true
 
 					daily_report.destroy
-					expect(@section_report.start_date).to be_nil
-					expect(@section_report.end_date).to be_nil
-					expect(@section_report.ongoing?).to be true
+					expect(section_report.start_date).to be_nil
+					expect(section_report.end_date).to be_nil
+					expect(section_report.ongoing?).to be true
 				end
 			end
 
 			context "first report" do
 				it "next report date should become section start date" do
-					2.times { create(:daily_report, section_report: @section_report) }
-					first_report = @section_report.daily_reports.first
-					second_report = @section_report.daily_reports.second
+					create_list(:daily_report, 2, section_report: section_report)
+					first_report = section_report.daily_reports.first
+					second_report = section_report.daily_reports.second
 
-					expect(@section_report.start_date).to eql(first_report.date)
+					expect(section_report.start_date).to eql(first_report.date)
 					first_report.destroy
 
-					expect(@section_report.start_date).to eql(second_report.date)
+					expect(section_report.start_date).to eql(second_report.date)
 				end
 			end
 		end
 	end
 
 	context "for last report of the section" do
-		before do
-			@section_report = create(:section_report)
-			4.times { create(:daily_report, section_report: @section_report, status: "ongoing") }
-		end
+		let!(:section_report) { create(:section_report) }
+		let!(:reports) { create_list(:daily_report, 4, section_report: section_report, status: "ongoing") }
 
 		context "on creation of" do
 			context "completed report" do
-				before do
-					@daily_report = create(:daily_report, section_report: @section_report, status: "completed")
-				end
+				let!(:daily_report) { create(:daily_report, section_report: section_report, status: "completed") }
+
 
 				it "should not change section report start date" do
-					expect(@section_report.start_date).not_to eql(@daily_report.date)
+					expect(section_report.start_date).not_to eql(daily_report.date)
 				end
 
 				it "should update section report end date" do
-					expect(@section_report.end_date).to eql(@daily_report.date)
+					expect(section_report.end_date).to eql(daily_report.date)
 				end
 
 				it "should update section report status" do
-					expect(@section_report.completed?).to be true
+					expect(section_report.completed?).to be true
 				end
 			end
 		end
@@ -183,72 +177,79 @@ RSpec.describe DailyReport do
 		context "on updating" do
 			context "status" do
 				context "completed report" do
-					before do
-						@daily_report = @section_report.daily_reports.order_by_date.first
-						expect(@section_report.end_date).to be_nil
-						expect(@section_report.ongoing?).to be true
+					let!(:daily_report) { section_report.daily_reports.order_by_date.first }
 
-						@daily_report.update(status: "completed")
+					before do
+						expect(section_report.end_date).to be_nil
+						expect(section_report.ongoing?).to be true
+						daily_report.update(status: "completed")
 					end
 
+
 					it "should update section report end date" do
-						expect(@section_report.end_date).to eql(@daily_report.date)
+						expect(section_report.end_date).to eql(daily_report.date)
 					end
 
 					it "should update section report status" do
-						expect(@section_report.completed?).to be true
+						expect(section_report.completed?).to be true
 					end
 				end
 
 				context "ongoing report" do
+					let!(:daily_report) {
+						create(:daily_report, section_report: section_report, status: "completed")
+						section_report.daily_reports.order_by_date.first
+					}
+
 					before do
-						create(:daily_report, section_report: @section_report, status: "completed")
-						@daily_report = @section_report.daily_reports.order_by_date.first
-						expect(@section_report.end_date).not_to be_nil
-						expect(@section_report.completed?).to be true
-						@daily_report.update(status: "ongoing")
+						expect(section_report.end_date).not_to be_nil
+						expect(section_report.completed?).to be true
+						daily_report.update(status: "ongoing")
 					end
 
 					it "should update section report end date" do
-						expect(@section_report.end_date).to be_nil
+						expect(section_report.end_date).to be_nil
 					end
 
 					it "should update section report status" do
-						expect(@section_report.ongoing?).to be true
+						expect(section_report.ongoing?).to be true
 					end
 				end
 			end
 
 			context "date of last completed report" do
-				before do
-					create(:daily_report, section_report: @section_report, status: "completed")
-					@daily_report = @section_report.daily_reports.order_by_date.first
-					expect(@section_report.end_date).not_to be_nil
-					expect(@section_report.completed?).to be true
+				let!(:daily_report) {
+					create(:daily_report, section_report: section_report, status: "completed")
+					section_report.daily_reports.order_by_date.first
+				}
 
-					@daily_report.update(date: Date.today + 1.month)
+				before do
+					expect(section_report.end_date).not_to be_nil
+					expect(section_report.completed?).to be true
+					daily_report.update(date: Date.today + 1.month)
 				end
 
 				it "should update section report end date" do
-					expect(@section_report.end_date).to eql(Date.today + 1.month)
+					expect(section_report.end_date).to eql(Date.today + 1.month)
 				end
 			end
 		end
 
 		context "on deleting last completed report" do
+			let!(:daily_report) { create(:daily_report, section_report: section_report, status: "completed") }
+
 			before do
-				daily_report = create(:daily_report, section_report: @section_report, status: "completed")
-				expect(@section_report.end_date).to eql(daily_report.date)
-				expect(@section_report.completed?).to be true
+				expect(section_report.end_date).to eql(daily_report.date)
+				expect(section_report.completed?).to be true
 				daily_report.destroy
 			end
 
 			it "should update section end date" do
-				expect(@section_report.end_date).to be_nil
+				expect(section_report.end_date).to be_nil
 			end
 
 			it "should update section status" do
-				expect(@section_report.ongoing?).to be true
+				expect(section_report.ongoing?).to be true
 			end
 		end
 	end
